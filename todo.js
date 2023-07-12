573ba57453a0e618e128f168bc2a2cd938573e45
malikabusufyan <118253617+malikabusufyan@users.noreply.github.com>
2023-07-12 21:31:25 +0530

    Final

--- todo.js
+++ todo.js
@@ -1,269 +1,98 @@
-
+// todo.js
 let tasks = [];
-const taskList = document.getElementById('list');
-const addTaskInput = document.getElementById('add');
-const tasksCounter = document.getElementById('tasks-counter');
+const taskList = document.getElementById("list");
+const addTaskInput = document.getElementById("add");
+const tasksCounter = document.getElementById("tasks-counter");
 
-function addTaskToDOM(task){
-    const li=document.createElement('li');
-    li.innerHTML = `
-              <input type="checkbox" id="${task.id}"  ${task.done ? 'checked' : ''} class="custom-checkbox">
-              <label for="${task.id}">"${task.text}"</label>
-              <img src="bin.jpeg" class="delete" data-id="${task.id}" />
-            `;
-    taskList.append(li);
+function addTaskToDOM(task) {
+  const li = document.createElement("li");
+  li.innerHTML = `
+    <input type="checkbox" id="${task.id}" ${task.done ? "checked" : ""} class="custom-checkbox">
+    <label for="${task.id}">${task.text}</label>
+    <img src="bin.jpeg" class="delete" data-id="${task.id}" />
+  `;
+  taskList.appendChild(li);
 }
 
-function renderList () {
-    taskList.innerHTML = '';
+function renderList() {
+  taskList.innerHTML = "";
 
-    for (let i=0; i<tasks.length; i++){
-        addTaskToDOM(tasks[i]);
-    }
-    tasksCounter.innerHTML = tasks.length;
+  for (let i = 0; i < tasks.length; i++) {
+    addTaskToDOM(tasks[i]);
+  }
+  tasksCounter.innerHTML = tasks.length;
 }
 
-function toggleTask (taskId) {
-    const task = tasks.filter(function (task){
-        return task.id === taskId;
-    });
+function toggleTask(taskId) {
+  const task = tasks.filter(function (task) {
+    return task.id === taskId;
+  });
 
-    if (task.length>0){
-        const currentTask=task[0];
+  if (task.length > 0) {
+    const currentTask = task[0];
 
-        currentTask.done = !currentTask.done;
-        renderList();
-        showNotification("Task Toggled Successfully");
-        return;
-    }
-    showNotification("Could not Toggle the Task")
-}
-
-function deleteTask (taskId) {
-    const newTasks = tasks.filter(function (task){
-        return task.id !== Number(taskId);
-    });
-
-    tasks=newTasks;
+    currentTask.done = !currentTask.done;
     renderList();
-    showNotification("Task deleted Successfully");
+    showNotification("Task Toggled Successfully");
+    return;
+  }
+  showNotification("Could not Toggle the Task");
 }
 
-function addTask (task) {
-    if (task){
-        tasks.push(task);
-        renderList();
-        showNotification("Task added successfully.");
-        return;
-    }
-    showNotification("Task cannot be Added.");
+function deleteTask(taskId) {
+  let newTasks = tasks.filter(function (task) {
+    return task.id !== taskId;
+  });
+
+  tasks = newTasks;
+  renderList();
+  showNotification("Task deleted Successfully");
+}
+
+function addTask(task) {
+  if (task) {
+    tasks.push(task);
+    renderList();
+    showNotification("Task added successfully.");
+    return;
+  }
+  showNotification("Task cannot be Added.");
 }
 
 function showNotification(text) {
-    alert(text);
+  alert(text);
 }
 
-function handleInputKeypress(e){
-    if (e.key === 'Enter'){
-        const text = e.target.value;
-        if (!text){
-            showNotification('Task text cannot be empty');
-            return;
-        }
-        const task = {
-            text,
-            id: Date.now().toString(),
-            done:false
-        }
-        e.target.value = '';
-        addTask(task);
+function handleInputKeypress(e) {
+  if (e.key === "Enter") {
+    const text = e.target.value;
+    if (!text) {
+      showNotification("Task text cannot be empty");
+      return;
     }
+    const task = {
+      text,
+      id: Date.now().toString(),
+      done: false,
+    };
+    e.target.value = "";
+    addTask(task);
+  }
 }
 
- function handleClickListener(e){
-        const target = e.target;
-        console.log(target);
+function handleClickListener(e) {
+  const target = e.target;
 
-        if (target.className === 'delete'){
-            const taskId = target.dataset.id;
-            deleteTask(taskId);
-        }
+  if (target.classList.contains("delete")) {
+    const taskId = target.dataset.id;
+    deleteTask(taskId);
+  }
 
-        if (target.className === 'custom-checkbox'){
-            const taskId = target.id;
-            toggleTask(taskId);
-        } 
-    }
+  if (target.classList.contains("custom-checkbox")) {
+    const taskId = target.id;
+    toggleTask(taskId);
+  }
+}
 
-addTaskInput.addEventListener('keyup', handleInputKeypress);
-document.addEventListener('click', handleClickListener);
-
-
-
-/*
-
-(function(){
-    let tasks = [];
-    const taskList = document.getElementById('list');
-    const addTaskInput = document.getElementById('add');
-    const tasksCounter = document.getElementById('tasks-counter');
-
-    console.log('Working');
-
-
-    async function fetchTodos(){
-        // fetch('https://jsonplaceholder.typicode.com/todos')
-        // .then(function(response){
-           
-        //     return response.json()
-
-        //     }).then(function(data){
-        //         tasks = data.slice(0,10);
-        //         renderList();
-        // })
-        // .catch(function(error){
-        //     console.log(error);
-        // })
-
-
-        try{
-            const response = await fetch('https://jsonplaceholder.typicode.com/todos');
-            const data = await response.json();
-            tasks = data.slice(0,10);
-            renderList();
-        }
-        catch(error){
-            console.log(error);
-        }
-    }
-    function addTaskToDOM(task){
-        const li = document.createElement('li');
-
-        li.innerHTML = `
-              <input type="checkbox" id="${task.id}"  ${task.completed ? 'checked' : ''} class="custom-checkbox">
-              <label for="${task.id}">"${task.title}"</label>
-              <img src="bin.jpeg" class="delete" data-id="${task.id}" />
-            `;
-
-        taskList.append(li);
-    }
-
-
-    function renderList () {
-        taskList.innerHTML = '';
-
-        for (let i =0; i<tasks.length;i++){
-            addTaskToDOM(tasks[i]);
-        }
-
-        tasksCounter.innerHTML = tasks.length;
-    }
-
-    function toggleTask(taskId) {
-        console.log(taskId);
-        const task = tasks.filter(function(task){
-            return task.id === Number(taskId);
-        });
-
-        if (task.length>0){
-            const currTask = task[0];
-
-            currTask.completed = ! currTask.completed;
-            renderList();
-            showNotification("Task toggled successfully!");
-            return;
-        }
-
-        showNotification("Could not toggle task!");
-    }
-
-    function deleteTask (taskId) {
-        const newTasks = tasks.filter(function(task){
-            return task.id !== Number(taskId);
-        })
-
-        tasks = newTasks;
-        renderList();
-        showNotification("Task deleted successfully!");
-    }
-
-    function addTask (task) {
-        // fetch('https://jsonplaceholder.typicode.com/todos', {
-        //     method : 'POST',
-        //     headers : {
-        //         'Content-Type' : 'application/json',
-        //     },
-        //     body : JSON.stringify(task);
-        // })
-        // .then(function(response){
-           
-        //     return response.json()
-
-        //     }).then(function(data){
-        //     tasks.push(task);
-        //     renderList();
-        //     showNotification("Task added successfully!");
-        //     return;
-        // })
-        // .catch(function(error){
-        //     console.log(error);
-        // })
-
-
-
-
-
-        tasks.push(task);
-        renderList();
-        showNotification("Task added successfully!");
-        return;
-    }
-
-    function showNotification(text) {
-        alert(text);
-    }
-
-    function handleInputKeypress(e){
-        if(e.key === 'Enter'){
-            const text = e.target.value;
-            console.log(text);
-
-            if(!text){
-                showNotification("The text cannot be empty")
-            }
-
-            const task={
-                title:text,
-                id: Date.now(),
-                completed:false
-            }
-            e.target.value='';
-            addTask(task);
-
-        }
-    }
-
-    function handleClickListener(e){
-        const target = e.target;
-        console.log(target);
-
-        if (target.className === 'delete'){
-            const taskId = target.dataset.id;
-            deleteTask(taskId);
-        }
-
-        if (target.className === 'custom-checkbox'){
-            const taskId = target.id;
-            toggleTask(taskId);
-        } 
-    }
-
-    function initializeApp(){
-        fetchTodos();
-        addTaskInput.addEventListener("keyup", handleInputKeypress);
-
-        document.addEventListener('click', handleClickListener);
-    }
-
-    initializeApp();
-})();*/
+addTaskInput.addEventListener("keyup", handleInputKeypress);
+taskList.addEventListener("click", handleClickListener);
